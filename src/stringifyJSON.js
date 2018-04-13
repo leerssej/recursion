0c4b5f679f2495738779409a79e95807f0e18dce
leerssej <jens.leerssen@gmail.com>
2018-04-12 23:41:58 -0700

    trimmed down stringifyJSON and cleaned out build tools

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,99 +3,59 @@
 
 // but you don't so you're going to write it from scratch:
 
-const stringifyJSON = (obj) => {
-  // base cases: all primitives (except for strings) =>
-    // convert to strings [no undefined in tests?]
-  // objects (arrays, objectLits) =>
-    // pull off braces/brackets and send elements back to base case
+const stringifyJSON = obj => {
+  // base cases: all primitives to string (strings wrapped in ")
+  // objects (null, arrays, objectLiterals) =>
+  // log braces/brackets/separators and send elements back to baseCase
 
-  // declare the string to begin building upon: assembledString;
+  // declare cases and string to begin building upon
   const baseCases = ['number', 'boolean'];
   const foulCases = ['functions', 'undefined'];
-  let assembledString = '';
+  let result = '';
 
   const baseCaseConverterOrHigherLevelSplitter = obj => {
-  //  if (!foulCases.includes(obj)) {
     // baseCase
     if (typeof obj !== 'object') {
-      // check if obj type is a base case ? T => add to assembled string
-      if (baseCases.includes(typeof obj)) return assembledString += obj.toString();
       // wrap strings in quotes
-      if (typeof obj === 'string') return assembledString += `"${obj}"`;
+      if (typeof obj === 'string') return result += `"${obj}"`;
+      // check if obj type is a base case ? T => add to assembled string
+      if (baseCases.includes(typeof obj)) return result += obj.toString();
+    }
+     
+    // higherNodes
+    // handle null = bugged up so needs special treatment: direct search
+    if (obj === null) return result += 'null';
       
-    } else { // higherNode // objects
-      // handle exception null = bugged up so needs special treatment: direct search
-      if (obj === null) return assembledString += 'null';
-      // if array - filet out each element and send up to base case again
-      if (Array.isArray(obj)) {
-        // handle empty array exception 
-        if (obj.length === 0) { 
-          return assembledString += '[]';
-        } else {
-          obj.forEach((val,i) => {
-            if (i === 0) assembledString += '['; // set opener
-            if (i > 0) assembledString += ','; // set separator
-            baseCaseConverterOrHigherLevelSplitter(val);
-          });
-          return assembledString += ']'; // close array up at end
-        }
-      } else { // only object subtype that remains is Object literal
-        // handle exception of empty object
-        if (Object.keys(obj).length === 0) {
-          return assembledString += '{}';
-        } else {
-          Object.entries(obj).forEach((kvPr, i) => {
-          // drop on braces at both ends
-            if (i === 0) assembledString += '{'; // set opener
-            if (!foulCases.includes(kvPr[0])) { // check if has a good key
-              // string out key value pairs
-              if (i > 0) assembledString += ','; // set separator
-              baseCaseConverterOrHigherLevelSplitter(kvPr[0]); // key
-              assembledString += ':'; 
-              baseCaseConverterOrHigherLevelSplitter(kvPr[1]); // value
-              }
-            }
-          });
-          return assembledString += '}'; // close array up at end
-        }
-      } // object literal 
-    } // all objects
-  //  }
-  }; // baseCaseConverterFunction
+    // array - filet out each element and send up to base case again
+    if (Array.isArray(obj)) {
+      // handle empty array exception 
+      if (obj.length === 0) return result += '[]';
+      
+      obj.forEach((val, i) => {
+        if (i === 0) result += '['; // set opener
+        if (i > 0) result += ','; // set separator
+        baseCaseConverterOrHigherLevelSplitter(val);
+      });
+      return result += ']'; // close array up at end
+    }
 
+    // Objects non-array
+    // handle exception of empty object
+    if (Object.keys(obj).length === 0) return result += '{}';
+    
+    Object.entries(obj).forEach((kvPr, i) => {
+      if (i === 0) result += '{'; // set opener
+      if (!foulCases.includes(kvPr[0])) { // check if has bad key
+        if (i > 0) result += ','; // set separator
+        baseCaseConverterOrHigherLevelSplitter(kvPr[0]); // key
+        result += ':';
+        baseCaseConverterOrHigherLevelSplitter(kvPr[1]); // value
+      }
+    });
+    return result += '}'; // close array up at end
+  }; 
   // feed the trunk into the chipper
   baseCaseConverterOrHigherLevelSplitter(obj);
-  // return all that was found    
-  return assembledString;
+
+  return result;
 };
-    
-const test = value => {
-      console.log(JSON.stringify(value), ': ', typeof (value), '  arr?:', Array.isArray(value));
-    }
-// test('hello world');
-
-const stringyObjects = [
-  9,
-  null,
-  true,
-  false,
-  'Hello world',
-  [],
-  [8],
-  ['hi'],
-  [8, 'hi'],
-  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-  [8, [[], 3, 4]],
-  [[[['foo']]]],
-  {},
-  { 'a': 'apple' },
-  { 'foo': true, 'bar': false, 'baz': null },
-  { 'boolean, true': true, 'boolean, false': false, 'null': null },
-  // basic nesting
-  { 'a': { 'b': 'c' } },
-  { 'a': ['b', 'c'] },
-  [{ 'a': 'b' }, { 'c': 'd' }],
-  { 'a': [], 'c': {}, 'b': true }
-];
-// stringyObjects.map(test);
-
