51a1b979adcf6d64786fbf2da3a96dbe425f6580
leerssej <jens.leerssen@gmail.com>
2018-04-13 13:04:23 -0700

    refactored components and fully annotated

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,35 +5,30 @@
 
 // But instead we're going to implement it from scratch:
 const getElementsByClassName = (targetClassName, node = document.body) => {
-  // traverse the DOM: when you hit a node with the specified className => add it to the foundNodeArr;
-
+  // traverse the DOM: inquiring about next level down each time.
+  // when you hit node with the specified className => add it to the foundNodeArr;
+  
   // declare collection variable;
   let foundNodeArr = [];
-
+  
+  // expand node collection to one level lower;
   let nodesBelow = node.childNodes;
+  //NBB: the inquiry into the children is what encourages function spawning
 
-  // const checkAndCollectNodesBeforeInquiringAboutTheirChildren = node => {
-    // deal with undefined
-    if (node.classList) {
-      console.log(node.classList.contains(targetClassName));
-      // checkIf Node has targetClassName
-      if (node.classList.contains(targetClassName)) foundNodeArr.push(node);
-    }
+  // deal with undefined
+  if (node.classList) {
+    // checkIf Node has targetClassName - if Y, then add to foundNodeArr; 
+    if (node.classList.contains(targetClassName)) foundNodeArr.push(node);
+  }
+  
+  // run everything at this next level down through the checker
+  nodesBelow.forEach(node => {
+    console.log(node);
+    foundNodeArr = [...foundNodeArr, ...getElementsByClassName(targetClassName, node)];
+  });
+  //NB: this is the Open Loop Maker that engenders recursion
 
-    // move to next level down
-
-    // run everything at this next level down through the checker
-    nodesBelow.forEach(node => {
-      console.log(node);
-      foundNodeArr = foundNodeArr.concat(getElementsByClassName(targetClassName, node));
-    });
-    //NB: this is the Open Loop Maker that engenders recursion
-    //NBB: the inquiry into the children is what encourages function spawning
-  // };
-
-  // oh yeah, make sure to start feeding the tree trunk into the wood-chipper
-  // checkAndCollectNodesBeforeInquiringAboutTheirChildren(document.body);
-
-  // return everything found
+  // return everything found 
+  // (if lower level recursion - this gets tacked onto foundNodeArr)
   return foundNodeArr;
 };
